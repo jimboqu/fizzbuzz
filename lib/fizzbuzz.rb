3286250128588b1cc505d2602b7e891656f7ce82
jimboqu <jamesm.quilter@gmail.com>
2018-12-13 11:35:46 +0000

    second test

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,5 +1,9 @@
 
 
 def fizzbuzz(number)
+  if number == 10
+    return "buzz"
+  end
+
   'fizz'
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -4,4 +4,7 @@ describe 'fizzbuzz' do
   it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
   end
+  it 'returns "buzz" when passed 10' do
+    expect(fizzbuzz(10)).to eq 'buzz'
+  end
 end
